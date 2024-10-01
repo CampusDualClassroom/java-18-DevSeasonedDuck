2e991b470be4c8317b57c052a6a21aa5f40161b8
Diego Alonso Cárcamo Gutiérrez <diegocarcamo.contacto@gmail.com>
2024-10-01 10:23:58 +0200

    Ejercicio completado

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -3,6 +3,28 @@ package com.campusdual.classroom;
 public class Exercise18 {
 
 	public static void main(String[] args) {
+		createAndInitializeArray(3);
+		showInlineArray(createAndInitializeArray(5));
 
 	}
+
+	public static int[] createAndInitializeArray(int n){
+		int [] array = new int[n];
+		int value=1;
+		for (int i=0; i<array.length;i++){
+			array[i]=value;
+			System.out.println(array[i]);
+			value++;
+		}
+		return array;
+	}
+
+	public static void showInlineArray(int[] array ){
+		String cadenaN="";
+		for (int i=0; i<array.length;i++){
+			cadenaN=cadenaN+(Integer.toString(array[i]))+(" ");
+		}
+		System.out.println(cadenaN.trim());
+	}
+
 }
